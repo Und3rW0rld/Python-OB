b923f70c518d03c220e70e3ebd54a447483d6949
Santiago Acevedo Cacua <santiago.acevedo@Quipux.local>
2023-01-21 12:08:35 -0500

    Solution excercice 8

---
+++ Excercice8Modules(1)/main.py
@@ -0,0 +1,10 @@
+import operaciones as o
+
+def Main():
+    print(o.suma(10, 11))
+    print(o.resta(11, 12))
+    print(o.multiplicacion(12, 10))
+    print(o.division(5, 5))
+
+if __name__ == '__main__':
+    Main()
